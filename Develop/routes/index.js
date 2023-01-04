f2eb5c4846047cc13dc72f77589c730eede29cd8
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-01-03 20:58:29 -0500

    diagnostic.js added

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -2,9 +2,11 @@ const express =require('express');
 
 //import modular routers for /notes
 const notesRouter=require('./notes');
+const diagnosticsRouter=require('./diagnostic');
 
 const app = express();
 
 app.use('./notes',notesRouter);
+app.use('./diagnostics', diagnosticsRouter);
 
 module.exports=app; 
