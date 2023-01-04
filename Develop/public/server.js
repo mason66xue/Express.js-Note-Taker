6acda68ab10f527c335a00117a00e0745a3d17d0
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-01-03 20:49:39 -0500

    diagnostic.js and json

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
+// GET Route for notes page
 app.get('/feedback', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
