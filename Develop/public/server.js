b4610b77073ded2e9de3b56a881d156b7259046b
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-01-03 22:11:57 -0500

    wild-card

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -29,6 +29,11 @@ app.get('/feedback', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
+
 
 //set up listner
 app.listen(PORT,()=> console.log('server started on port '))
