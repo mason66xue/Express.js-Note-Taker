f52282b8dcefefa0d30e8daf54f04a05b96dfdd6
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-01-22 15:17:02 -0500

    quick fix html

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path=require('path');
+const app=express();
+const api = require('./routes/index.js');
+
+const PORT =process.env.PORT || 3001;
+
+
+
+//set a static folder 
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+// localhost:3001/
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+// GET Route for notes page
+// localhost:3001/notes
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Wildcard route to direct users to home
+// localhost:3001/anything else
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/develop/public/index.html'))
+// );
+
+
+//set up listner
+app.listen(PORT,()=> console.log(`server started on port ${PORT}`))
+
+
